8e3fee9f8bb5118a640f8ad1fdd250e6e18ba5b9
Noah Scholfield <contact@noahscholfield.com>
2017-11-09 21:43:16 -0500

    Try adding box upload support

--- js/md-editor.js
+++ js/md-editor.js
@@ -1,7 +1,7 @@
 /* global Vue: false, markdownit: false */
 var md = markdownit({html: true});
 var reader = new FileReader();
-reader.onload = (event) => test.markdown = event.target.result;
+reader.onload = (event) => mdEditor.markdown = event.target.result;
 
 var mdEditor = new Vue({
   el: '#md-editor',
@@ -22,6 +22,23 @@ var mdEditor = new Vue({
     processFile(event) {
       this.fileInfo = event.target.files[0];
       reader.readAsText(event.target.files[0]);
+    },
+    uploadFileToBox() {
+      var uploadPath = `https://upload.box.com/api/2.0/files/${this.fileInfo.id}/content`;
+      var form = new FormData();
+      var file = new Blob([this.markdown], {type: 'text/markdown'});
+      form.append('content_modified_at', Date.now());
+      form.append('file', file);
+      fetchPost(uploadPath, form);
     }
   }
 });
+
+function fetchPost(url, data) {
+  fetch(url, {
+    method: 'post',
+    body: data
+  })
+    .then((response) => console.log(response.data))
+    .catch((response) => console.log(response));
+}
